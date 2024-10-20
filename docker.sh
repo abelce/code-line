db0a2201cda17ec2fff5770d19d0d8f69243f88d
tangzx <1061225829@qq.com>
2024-10-20 17:54:09 +0800

    feat: 🎸 添加docker配置

---
+++ docker.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+ docker build -t abelce/code-line:1.0.0 .
+ docker push abelce/code-line:1.0.0
---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 将环境变量添加到locale中
+# env >> /etc/default/locale
+
+# 启动crontab
+# /etc/init.d/cron start
+
+# corepack enable pnpm && pnpm i
+
+node ./server.js 
