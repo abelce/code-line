3f66398e7f5d6d7b864e169e34922ba78482b13d
tangzx <1061225829@qq.com>
2024-10-23 14:02:00 +0800

    feat: 🎸 删除代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,24 +60,7 @@ export default function RootLayout({
       <head>
         {isDev ? null : (
           <>
-            <meta
-              name="google-site-verification"
-              content="QNgsHzNBqTCrK75m-fCBXU2ZyISQotf5SxYmHPKMZ80"
-            />
             <GoogleAnalytics gaId="G-E8TJS0CFGQ" />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-          var _hmt = _hmt || [];
-        (function() {
-          var hm = document.createElement("script");
-          hm.src = "https://hm.baidu.com/hm.js?87a3ab412b5c6b8bba008262d7ca42e8";
-          var s = document.getElementsByTagName("script")[0]; 
-          s.parentNode.insertBefore(hm, s);
-        })()
-          `,
-              }}
-            ></script>
           </>
         )}
       </head>
