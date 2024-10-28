82965ba6c53812976acffdb4730ca671ea580a54
tangzx <1061225829@qq.com>
2024-10-29 01:06:57 +0800

    feat: 🎸 loading添加动画

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,8 +2,8 @@ import { Loader } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="h-full w-full flex justify-center items-center">
-      <Loader />
+    <div className="h-full w-full flex justify-center items-center p-8">
+      <Loader className="animate-spin"/>
     </div>
   );
 }
