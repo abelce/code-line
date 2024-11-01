ce6cce149f4b3a20980c114365beb0c21ca8018f
tangzx <1061225829@qq.com>
2024-11-01 15:16:54 +0800

    fix: 🐛 修复代码显示被截断问题

--- app/[locale]/embed/page.tsx
+++ app/[locale]/embed/page.tsx
@@ -3,10 +3,18 @@ import { Mode } from "../code-line/Editor/inde";
 import Frame from "../code-line/Frame";
 import useGetInitState from "@/hooks/useGetInitState";
 import { BgType } from "../code-line/Setting/Backdrop";
+import { useEffect } from "react";
 
 const Content = () => {
   const { code, padding, lang, theme, title, backdropType, backdrop, copyBtn, lineNum } =
     useGetInitState();
+
+    useEffect(() => {
+      const body = document.querySelector("body");
+      if (body) {
+        body.style.background = "transparent";
+      }
+    }, [])
 
   return (
     <div className="w-screen h-screen">
